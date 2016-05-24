663d8a1dc38ba9a1077401fde1be3b2e4dfd2c83
钟颖 <cyan.zy@alibaba-inc.com>
2016-05-24 12:27:40 +0800

    Add sample

--- GCDThrottle/ViewController.m
+++ GCDThrottle/ViewController.m
@@ -12,9 +12,22 @@
 @implementation ViewController
 
 - (IBAction)textFieldValueChanged:(UITextField *)sender {
+    
     dispatch_throttle(0.3, ^{
         NSLog(@"search: %@", sender.text);
     });
+    
+    dispatch_throttle_on_queue(0.3, THROTTLE_GLOBAL_QUEUE, ^{
+        NSLog(@"search: %@", sender.text);
+    });
+    
+    [GCDThrottle throttle:0.3 block:^{
+        NSLog(@"search: %@", sender.text);
+    }];
+    
+    [GCDThrottle throttle:0.3 queue:THROTTLE_GLOBAL_QUEUE block:^{
+        NSLog(@"search: %@", sender.text);
+    }];
 }
 
 @end
